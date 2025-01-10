18b3101285168050003e26b1a937226c3c3a25c5
Dazled <danielw0703@gmail.com>
2025-01-10 12:55:38 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -16,10 +16,3 @@ app.use('/api', apiRouter);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
-app.get('/', (req, res) => {
-    res.send('Aplikasi Railway berjalan!');
-});
-
-app.listen(PORT, () => {
-    console.log(`Server berjalan di port ${PORT}`);
-});
